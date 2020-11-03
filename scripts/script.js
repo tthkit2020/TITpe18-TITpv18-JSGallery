106bbbbe0a29b7b4cdff4520e26dccf438f7e17a
Julia Aasmaa <my_e_10@hotmail.com>
2020-11-03 15:42:12 +0200

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("Hello from js file");
+//console.log("Hello from js file");
+const currentImage = document.getElementById("current");
+console.log(currentImage);
+const galleryImages = document.querySelectorAll(".img");
+console.log(galleryImages);
+
+galleryImages.forEach(img => {
+    img.addEventListener("click", (event) => {
+        galleryImages.forEach(img => {
+            img.style.opacity = 1;
+        });
+        currentImage.src = event.target.src;
+        event.target.style.opacity = 0.6;
+    });
+});
